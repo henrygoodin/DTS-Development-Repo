23aef79babd15173f53d5b3d5a2110f3e07fcea5
henrygoodin <143525282+henrygoodin@users.noreply.github.com>
2025-03-06 09:46:06 -0600

    Update index,js Cors
    Henry Goodin

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ app.use(express.static('frontend/dist'));
 const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
-    'https://dts-development-frontend.onrender.com'
+    'https://dts-development-frontend.onrender.com',
+    'https://www.dtsdevelopments.com'
 ];
 
 app.use(cors({
